c3d4a54ff20ca7c828f93add86622b0d675b8c76
Muhammad Soliman <m.solimanz@hotmail.com>
2017-10-13 09:58:39 -0500

    counting, bucket sorts

--- src/com/alkhawarizms/Sorting/Bucket.java
+++ src/com/alkhawarizms/Sorting/Bucket.java
@@ -1,7 +1,73 @@
 package com.alkhawarizms.Sorting;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by msoliman on 4/13/17.
+ * Not in place
+ * Description:
+ * Depends on another bucket/array that distribute the values in buckets, we must know in advance that items are fairly distributed over intervals. we should divide this interval in K equal-sized (buckets)
+ * then for every bucket we use some linear sorting algoorithm (e.g. Counting Sort)
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Steps:
+ * Create a bucket array,
+ * Start t put all items in each bucket (by dividing on length of bucket array),
+ * Put Collisioned items in LinkedList/Array
+ * Sort each bucket's content in case it has many items in each bucket (using insertion sort)
+ * Start to list all items from buckets
+ * Algorithm:
+ *  For each element i from L
+        If B[i] is not empty
+            Put A[i] into B[i] using insertion sort;
+        Else B[i] := A[i]
+    Concatenate B[i .. n] into one sorted list;
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Time Complexity: O(n+k) where n is the number of items in the array to be sorted and K is the number of buckets
  */
 public class Bucket {
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(new Bucket().sort(new int[]{1,6,3,78,12,3,5,1,3}, 9)));
+
+    }
+
+    public int[] sort(int[] items, int bk){
+        if(items == null || items.length == 0)
+            return  null;
+
+        //list or arraylist
+        List<Integer>[] buckets = new ArrayList[bk];
+
+        for(int i: items){
+            //find where to save ... its bucket (space)
+            int indx = (int) Math.floor(i/bk);
+            if(buckets[indx] == null){
+                buckets[indx] = new ArrayList<>();
+            }
+            buckets[indx].add(i);
+        }
+
+        //insertion sort for all ArrayList you have added into each bucket
+        int indx = 0;
+        for(List<Integer> bucket: buckets){
+            if(bucket != null){
+                //sort first, items should be sorted using some linear sorting algorithm, we can use CountingSort O(n)
+                //Collections.sort(bucket) can be used for simplicity too - Insertion Sort can be used for small sized arrays
+                int[] sorted = Counting.sort(bucket.stream().filter( t -> t!=null).mapToInt(t -> t).toArray());
+
+                //list them for output
+                for(int i : sorted){
+                    items[indx++] = i;
+                }
+
+            }
+        }
+
+
+        return items;
+    }
+
 }
--- src/com/alkhawarizms/Sorting/Counting.java
+++ src/com/alkhawarizms/Sorting/Counting.java
@@ -5,24 +5,46 @@ import java.util.HashMap;
 
 /**
  * Created by msoliman on 4/19/17.
- * O(n), Stable, Not in place
+ * O(n), Stable, Not in place as it needs another array for preserving counts
  * Description:
  *  Counting sort is a simple algorithm where we are having a nother array or storage for preserving the counts of each element in the array
- *  then start calculating the
+ *  then start distributing or populating the values according to those counters
+ *  ----------------------------------------------------------------------------------------------------------------------------------------------------------------
  */
 public class Counting {
+
+    public  static int[] sort(int[] items){
+        if(items == null || items.length == 0)
+            return null;
+
+        //get the upper bound (max)
+        //Linear Time O(n)
+        int max = items[0];
+        for(int i = 1; i<items.length; i++){
+            if(items[i] > max)
+                max = items[i];
+        }
+
+        return sort(items, max);
+    }
+
+
     public static int[] sort(int[] items, int bound) {
         return countingSort(items, bound);
     }
 
     private static int[] countingSort(int[] items, int bound) {
+        //instantiate an array for preserving counts of each element in the array
         int[] counts = new int[bound+1];
+
+        //initialize counters with zeros (all items in counters array) O(k) K is the upper bound of counts
+        //As long as K is less than or equal to n, it is okay however if K is so bigger than n it will badly affect on time complexity
         for (int i = 0; i < counts.length; i++)
             counts[i] = 0;
 
-        //how many times an elem occured in the array
-        for (int i = 0; i < items.length; i++) {
-            counts[items[i]] = counts[items[i]] + 1;
+        //collecting: how many times an elem occurred/encountered in the array
+        for (int item : items) {
+            counts[item] = counts[item] + 1;
         }
 
         //count the occurrence of prefixes or preceding items for each item
@@ -31,11 +53,10 @@ public class Counting {
             for (int i = 1; i < counts.length; i++)
                 counts[i] = counts[i] + counts[i - 1];
 
-        //
         int[] results = new int[items.length];
-        for (int i = 0; i < items.length; i++) {
-            results[counts[items[i]] - 1] = items[i];
-            counts[items[i]] = counts[items[i]] - 1;
+        for (int item : items) {
+            results[counts[item] - 1] = item;
+            counts[item] = counts[item] - 1;
         }
 
         return results;
