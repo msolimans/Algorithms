62a3a3ee459d2168986f5741d986f43ab466474e
Muhammad Soliman <msuliman@mum.edu>
2019-05-07 02:22:53 -0400

    MIT-6006: Find Peak Element in 2D Array using DivideAndConquer Algorithm

--- src/com/ocw/c6006/lect1/FindPeakIn2DArray.java
+++ src/com/ocw/c6006/lect1/FindPeakIn2DArray.java
@@ -14,17 +14,18 @@ public class FindPeakIn2DArray {
         System.out.println(GreedyAscentAlgorithm(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
     }
 
+    //worst case scanario O(n*m) if n = m this will result in O(n^2)
     static int GreedyAscentAlgorithm(int[][] arr) {
         int rows = arr.length;
         int cols = arr[0].length;
 
         int midi = rows / 2;
         int midj = cols / 2;
-        return GreeAscentAlgorithm(arr, midi, midj);
+        return GreedyAscentAlgorithm(arr, midi, midj);
     }
 
 
-    static int GreeAscentAlgorithm(int[][] arr, int midi, int midj) {
+    static int GreedyAscentAlgorithm(int[][] arr, int midi, int midj) {
 
 
         boolean pass = true;
@@ -68,7 +69,7 @@ public class FindPeakIn2DArray {
             nmidj = midj;
         }
 
-        return GreeAscentAlgorithm(arr, nmidi, nmidj);
+        return GreedyAscentAlgorithm(arr, nmidi, nmidj);
     }
 
     @Test
@@ -77,4 +78,51 @@ public class FindPeakIn2DArray {
         Assert.assertEquals(20, GreedyAscentAlgorithm(new int[][]{{0, 0, 10, 15}, {14, 13, 12, 0}, {15, 0, 11, 17}, {17, 18, 19, 20}}));
     }
 
+
+    //T(n) = T(m/2) + O(n)
+    //T(1) = O(n)
+    //T(n) = O(n) + O(n) + O(n) ..... [log(m) times]
+    //O(n*log m) where n number of rows and m number of cols
+    static  int DivideAndConquerAlgorithm(int[][] arr) {
+        return DivideAndConquerAlgorithm(arr, 0, arr[0].length - 1);
+    }
+
+    static int DivideAndConquerAlgorithm(int[][] arr, int js, int je) {
+
+        int mid = (js + je) / 2;
+
+        int i = IndexOfMaxInColumn(arr, mid);
+        int njs = js;
+        int nje = je;
+
+        if (mid - 1 >= 0 && arr[i][mid - 1] > arr[i][mid])
+            nje = mid - 1;
+        if (mid + 1 <= arr.length - 1 && arr[i][mid + 1] > arr[i][mid])
+            njs = mid + 1;
+
+        if (je == nje && js == nje)
+            return arr[i][mid];
+
+        return DivideAndConquerAlgorithm(arr, njs, nje);
+    }
+
+
+    static  int IndexOfMaxInColumn(int[][] arr, int j) {
+        int max = arr[0][j];
+        int imax = 0;
+        for (int i=1;i<arr.length;i++){
+            if (arr[i][j] > max){
+                max = arr[i][j];
+                imax = i;
+            }
+        }
+
+        return imax;
+    }
+
+    @Test
+    public void TestDivideAndConquerAlgorithm(){
+        Assert.assertEquals(9, DivideAndConquerAlgorithm(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+        Assert.assertEquals(20, DivideAndConquerAlgorithm(new int[][]{{0, 0, 10, 15}, {14, 13, 12, 0}, {15, 0, 11, 17}, {17, 18, 19, 20}}));
+    }
 }
