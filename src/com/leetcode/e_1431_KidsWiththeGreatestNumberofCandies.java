3290e2cd2046afa9b8acec52082061d2a0a8dd6a
Muhammad Soliman <m.solimanz@hotmail.com>
2021-03-30 12:46:02 -0400

    1431. Kids With the Greatest Number of Candies

---
+++ src/com/leetcode/e_1431_KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,68 @@
+package com.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+//1431. Kids With the Greatest Number of Candies
+//Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+//
+//For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+//
+//
+//
+//Example 1:
+//
+//Input: candies = [2,3,5,1,3], extraCandies = 3
+//Output: [true,true,true,false,true]
+//Explanation:
+//Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies --- the greatest number of candies among the kids.
+//Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids.
+//Kid 3 has 5 candies and this is already the greatest number of candies among the kids.
+//Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies.
+//Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids.
+//Example 2:
+//
+//Input: candies = [4,2,1,1,2], extraCandies = 1
+//Output: [true,false,false,false,false]
+//Explanation: There is only 1 extra candy, therefore only kid 1 will have the greatest number of candies among the kids regardless of who takes the extra candy.
+//Example 3:
+//
+//Input: candies = [12,1,12], extraCandies = 10
+//Output: [true,false,true]
+//
+//
+//Constraints:
+//
+//2 <= candies.length <= 100
+//1 <= candies[i] <= 100
+//1 <= extraCandies <= 50
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+public class e_1431_KidsWiththeGreatestNumberofCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int i: candies){
+            if (i > max )
+                max = i;
+        }
+
+        List<Boolean> result = new ArrayList<>();
+
+        for(int i:candies){
+            if (i+extraCandies >= max)
+                result.add(true);
+            else result.add(false);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void TestKidsWithCandies() {
+        Assert.assertArrayEquals(new Boolean[]{true,true,true,false,true}, kidsWithCandies(new int[]{2,3,5,1,3}, 3).toArray());
+        Assert.assertArrayEquals(new Boolean[]{true,false,false,false,false}, kidsWithCandies(new int[]{4,2,1,1,2}, 1).toArray());
+        Assert.assertArrayEquals(new Boolean[]{true}, kidsWithCandies(new int[]{4}, 0).toArray());
+    }
+
+}
